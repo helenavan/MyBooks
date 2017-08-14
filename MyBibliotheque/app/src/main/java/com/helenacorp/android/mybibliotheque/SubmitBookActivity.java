7503f2a2ed5be571ee84157efb005ab42e4fe0fd
helenavan <tedone.bd@gmail.com>
2017-08-14 17:22:37 +0200

    submitBookActivity

--- MyBibliotheque/app/src/main/java/com/helenacorp/android/mybibliotheque/SubmitBookActivity.java
+++ MyBibliotheque/app/src/main/java/com/helenacorp/android/mybibliotheque/SubmitBookActivity.java
@@ -1,13 +1,85 @@
 package com.helenacorp.android.mybibliotheque;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class SubmitBookActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SubmitBookActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText titleName, firstName, lastName;
+    private Button btnClean, btnAdd, btnPic;
+    private ProgressBar progressBar;
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit_book);
+
+        titleName = (EditText) findViewById(R.id.title_submit);
+        firstName = (EditText) findViewById(R.id.nameAutor_submit);
+        lastName = (EditText) findViewById(R.id.autorLastName_submit);
+        btnClean = (Button) findViewById(R.id.btn_clean_submit);
+        btnAdd = (Button) findViewById(R.id.btn_add_submit);
+        btnPic = (Button) findViewById(R.id.btn_photoBook_submit);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnAdd.setOnClickListener(this);
+        btnClean.setOnClickListener(this);
+        btnPic.setOnClickListener(this);
     }
+
+    public void validation() {
+        if (titleName.getText().length() == 0 || firstName.length() == 0) {
+            Context context = getApplicationContext();
+            CharSequence text = "Ha! N'avez-vous pas oublié quelque chose?";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        } else {
+            sendBook(btnAdd);
+        }
+    }
+
+    public void sendBook(View view) {
+        user = mAuth.getCurrentUser();
+        String userName = user.getDisplayName();
+        //write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("book");
+        BookModel bookModel = new BookModel(titleName.getText().toString(), null, null, firstName.getText().toString(),
+                lastName.getText().toString(), null, null);
+        ref.push().setValue(bookModel);
+    }
+
+    public void clearEditText(EditText editText) {
+        editText.setText("");
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == btnAdd) {
+            sendBook(view);
+        }
+        if (view == btnClean) {
+            clearEditText(titleName);
+            clearEditText(firstName);
+            clearEditText(lastName);
+        }
+        if (view == btnPic) {
+
+        }
+    }
+
 }
